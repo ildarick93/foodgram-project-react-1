aa9727991d518232f62ddf25a87b3b56fca2a02f
lisvv <lisyansky1234@yandex.ru>
2021-10-06 11:41:10 +0300

    test

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -120,11 +120,12 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
         for ingredient in ingredients:
             ingredient_id = ingredient['ingred']['id']
             amount = ingredient['amount']
+            ingred_instance = Ingredient.objects.get(id=ingredient_id)
             if IngredientAmount.objects.\
                filter(recipe_id=recipe, ingred_id=ingredient_id).exists():
                 amount += F('amount')
             ingred = IngredientAmount.objects.update_or_create(
-                recipe_id=recipe, ingred_id=ingredient_id,
+                recipe_id=recipe, ingred=ingred_instance,
                 defaults={'amount': amount})
             ingredients_list.append(ingred)
         return ingredients_list
